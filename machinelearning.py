f73bbc902c951103e43e1ac87c24a3f7fd80f32e
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-30 07:43:11 +0700

    up

--- machinelearning.py
+++ machinelearning.py
@@ -1,13 +1,33 @@
 import json
 
+
+class Sentiment:
+    Negative = 'Negative'
+    Neutral = 'Neutral'
+    Positive = 'Positive'
+
+
+class Review:
+    def __init__(self, text, score):
+        self.text = text
+        self.score = score
+        self.sentiment = self.get_sentiment()
+    
+    def get_sentiment(self):
+        if self.score <= 2:
+            return Sentiment.Negative
+        elif self.score == 3:
+            return Sentiment.Neutral
+        else:
+            return Sentiment.Positive
+
+
 file_name = 'D:\Phyton Code\Contoh dari Github\sklearn-master\data\sentiment\Books_small.json'
 
 comment = []
 with open(file_name) as f:
     for line in f:
         review = json.loads(line)
-        comment.append((review['reviewText'], review['overall']))
-        print(comment)
+        comment.append(Review(review['reviewText'], review['overall']))
 
-
-comment[5]
+print(comment[5].sentiment)
