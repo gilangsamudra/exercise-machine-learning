f897332a4f54f19461379fd0f1ce9c1682aff92f
Gilang <gilang.samudrakuswana@gmail.com>
2020-04-30 12:05:37 +0700

    update

--- machinelearning.py
+++ machinelearning.py
@@ -1,10 +1,12 @@
 import json
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 class Sentiment:
-    Negative = 'Negative'
-    Neutral = 'Neutral'
-    Positive = 'Positive'
+    Negative = 'NEGATIVE'
+    Neutral = 'NEUTRAL'
+    Positive = 'POSITIVE'
 
 
 class Review:
@@ -31,3 +33,21 @@ with open(file_name) as f:
         comment.append(Review(review['reviewText'], review['overall']))
 
 print(comment[5].sentiment)
+
+
+# Prep Data
+training, test = train_test_split(comment, test_size=0.33, random_state=42)
+print(training[0].text)
+
+train_x = [x.text for x in training]
+train_y = [x.sentiment for x in training]
+print(train_y[0])
+
+test_x = [x.text for x in test]
+test_y = [x.sentiment for x in test]
+print(test_y[0])
+
+# Bag of Words Vectorization
+count_vect = CountVectorizer()
+train_x_vector = count_vect.fit_transform(train_x)
+print(train_x_vector[0].toarray())
