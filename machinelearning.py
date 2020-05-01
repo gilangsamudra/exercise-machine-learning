c367648892644adc25adb5b5bd933ad0586e1f93
Gilang <gilang.samudrakuswana@gmail.com>
2020-05-01 14:34:39 +0700

    update

--- machinelearning.py
+++ machinelearning.py
@@ -1,4 +1,5 @@
 import json
+import random
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import svm
@@ -6,6 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
+
 
 class Sentiment:
     Negative = 'NEGATIVE'
@@ -18,7 +20,7 @@ class Review:
         self.text = text
         self.score = score
         self.sentiment = self.get_sentiment()
-    
+
     def get_sentiment(self):
         if self.score <= 2:
             return Sentiment.Negative
@@ -28,7 +30,25 @@ class Review:
             return Sentiment.Positive
 
 
-file_name = 'D:\Phyton Code\Contoh dari Github\sklearn-master\data\sentiment\Books_small.json'
+class ReviewContainer:
+    def __init__(self, reviews):
+        self.reviews = reviews
+
+    def get_text(self):
+        return [x.text for x in self.reviews]
+
+    def get_sentiment(self):
+        return [x.sentiment for x in self.reviews]
+
+    def evenly_distribute(self):
+        negative = list(filter(lambda x: x.sentiment == Sentiment.Negative, self.reviews))
+        positive = list(filter(lambda x: x.sentiment == Sentiment.Positive, self.reviews))
+        positive_shrunk = positive[:len(negative)]
+        self.reviews = negative + positive_shrunk
+        random.shuffle(self.reviews)
+
+
+file_name = 'D:\Phyton Code\Contoh dari Github\sklearn-master\data\sentiment\Books_small_10000.json'
 
 comment = []
 with open(file_name) as f:
@@ -40,16 +60,26 @@ print(comment[5].sentiment)
 
 
 # Prep Data
+# split the data into training and test data
 training, test = train_test_split(comment, test_size=0.33, random_state=42)
 print(training[0].text)
 
-train_x = [x.text for x in training]
-train_y = [x.sentiment for x in training]
-print(train_y[0])
+# filter the number of training data to be evenly distributed
+train_container = ReviewContainer(training)
+test_container = ReviewContainer(test)
 
-test_x = [x.text for x in test]
-test_y = [x.sentiment for x in test]
-print(test_y[0])
+train_container.evenly_distribute()
+test_container.evenly_distribute()
+
+train_x = train_container.get_text()
+train_y = train_container.get_sentiment()
+
+test_x = test_container.get_text()
+test_y = test_container.get_sentiment()
+
+print(train_y.count(Sentiment.Positive))
+print(train_y.count(Sentiment.Negative))
+
 
 # Bag of Words Vectorization
 count_vect = CountVectorizer()
@@ -94,4 +124,8 @@ print(f1_score(test_y, clf_dec.predict(test_x_vector), average=None, labels=[Sen
 print(f1_score(test_y, clf_gnb.predict(test_x_vector.toarray()), average=None, labels=[Sentiment.Positive, Sentiment.Neutral, Sentiment.Negative]))
 print(f1_score(test_y, clf_lg.predict(test_x_vector), average=None, labels=[Sentiment.Positive, Sentiment.Neutral, Sentiment.Negative]))
 
-# Istirahat dulu
+# Improve F1 Score
+print(train_x[0])
+print(train_y[:5])
+print(train_y.count(Sentiment.Negative))
+# Balance the negative and positive data
